2488d8ac3c35c5500d0c6487a01c9ea5c7c48bcb
Felix Mumme <felix.mumme@mumme-it.de>
2024-12-14 19:24:38 +0100

    add botbucket chat api

--- src/pages/modules/ModuleOne.tsx
+++ src/pages/modules/ModuleOne.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Button, Typography} from "@mui/material";
 
 
 const ModuleOne: React.FC = () => {
+
+
     return (
         <div>
             <h1>Module One</h1>
             <h2>Make this fancy!</h2>
+
         </div>
     );
 };
